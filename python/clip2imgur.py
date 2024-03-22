17b1dd3783a468df2e1674bcd013aed74cc1c0ab
Jay Wang <jay@zijie.wang>
2024-03-22 01:39:46 -0400

    Add post_image

--- python/clip2imgur.py
+++ python/clip2imgur.py
@@ -2,10 +2,11 @@ from PIL import Image
 from matplotlib import pyplot as plt
 from typing import Union, Optional
 
-import io
 import AppKit
 import requests
 import base64
+
+CLIENT_ID = "db7dd95cd20fa48"
 
 
 def get_clipboard_image() -> Optional[bytes]:
@@ -28,3 +29,38 @@ def get_clipboard_image() -> Optional[bytes]:
     # Create an image from the data
     data = raw_data.bytes().tobytes()
     return data
+
+
+def post_image(image_data: bytes) -> str:
+    """
+    Posts an image to Imgur using the provided image data.
+
+    Args:
+        image_data (bytes): The image data to be uploaded.
+
+    Returns:
+        str: The URL of the uploaded image.
+
+    Raises:
+        ValueError: If the response from Imgur is not successful.
+    """
+
+    image_base64 = base64.b64encode(image_data)
+
+    # Build the requests
+    auth_value = f"Client-ID {CLIENT_ID}"
+    headers = {"Authorization": auth_value, "Cache-Control": "no-cache"}
+    parameters = {
+        "image": (None, image_base64),
+        "type": (None, "base64"),
+    }
+    url = "https://api.imgur.com/3/image"
+
+    # Get the results
+    response = requests.post(url, headers=headers, files=parameters, timeout=120)
+    data = response.json()
+
+    if response.status_code == 200:
+        return data["data"]["link"]
+    else:
+        raise ValueError(data)
