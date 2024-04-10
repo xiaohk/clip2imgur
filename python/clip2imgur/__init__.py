9c825d2e3dd023658b4f0c6b49a8518652f4c113
Jay Wang <jay@zijie.wang>
2024-04-09 22:44:07 -0400

    Bump version: 0.9.1 → 0.9.2

--- python/clip2imgur/__init__.py
+++ python/clip2imgur/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Jay Wang"""
 __email__ = "jayw@zijie.wang"
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 from clip2imgur.clip2imgur import *
--- python/setup.py
+++ python/setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/xiaohk/clip2imgur",
-    version="0.9.1",
+    version="0.9.2",
     zip_safe=False,
     entry_points={
         "console_scripts": [
